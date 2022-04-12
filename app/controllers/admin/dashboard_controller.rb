940cfc10c4c0f1f5c66f569b79a86064b3f32ced
Johannes Weber <johannes@drweber.de>
2022-04-12 16:00:31 -0700

    set instance variables with dashboard data

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.count
+    @categories = Category.count
+    @total_stock = Product.all.reduce(0) do |sum, n|
+      sum = sum + n.quantity
+    end
   end
 end
