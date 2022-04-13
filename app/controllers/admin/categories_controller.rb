0bb21e0e0b6328d2a7101ef82a3aaec4613b3f83
Johannes Weber <johannes@drweber.de>
2022-04-12 20:04:23 -0700

    add basic_auth to admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["BASIC_AUTH_USER"], password: ENV["BASIC_AUTH_PASSWORD"]
+
   def index
     @categories = Category.all
   end
