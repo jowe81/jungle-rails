fae9af9ecd1934eafd25ba0f7378863529877ffc
Johannes Weber <johannes@drweber.de>
2022-04-14 08:35:12 -0700

    make user an instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id #log the user in
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id #log the user in
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render 'new'
     end
   end
 
