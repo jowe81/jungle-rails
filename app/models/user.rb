cd94021f0478c0d0b18203c4302c8ec92581aa3e
Johannes Weber <johannes@drweber.de>
2022-04-14 08:51:40 -0700

    add more validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
