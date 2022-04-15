11c0153754b9f95c9aab043bc7ba5c5e0f44c9d6
Johannes Weber <johannes@drweber.de>
2022-04-15 11:35:08 -0700

    refactor to only support one active sale at a time

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,8 +1,8 @@
 class Sale < ActiveRecord::Base
 
-  # AR Scope - get currently active sales records
+  # AR Scope - get (first) currently active sales record
   def self.active
-    where("sales.starts_on < ? AND sales.ends_on >= ?", Date.current, Date.current)
+    where("sales.starts_on < ? AND sales.ends_on >= ?", Date.current, Date.current).first
   end
 
   # Alternate way to define AR Scope
