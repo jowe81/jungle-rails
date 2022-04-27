4f74ea95f4c7bd5525e3720b1c112399dd2e0bba
Johannes Weber <johannes@drweber.de>
2022-04-26 18:32:12 -0700

    add self.authenticate_width_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
+  def self.authenticate_with_credentials email, password
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
