a41afbeb2be9645219ac7c12802ee58dd88235d5
Johannes Weber <johannes@drweber.de>
2022-04-26 18:53:21 -0700

    save email in lowercase on new users, support surrounding whitespace and mixed case for email on authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  before_save :downcase_email
 
   def self.authenticate_with_credentials email, password
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       user
     end
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase!
   end
 
 end
