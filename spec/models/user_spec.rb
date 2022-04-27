9ea3eba7814eb2d9c8488cf4d3d22f0dfb1890b0
Johannes Weber <johannes@drweber.de>
2022-04-26 18:32:29 -0700

    add specs for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+
+  describe "Validations" do
+
+    before(:each) do
+      @valid_test_record = {
+        :name => "Bob",
+        :email => "bob@gmail.com",
+        :password => "password",
+        :password_confirmation => "password"
+      }
+    end
+
+    context "When attempting to generate a user" do
+
+      it "fails if no email is provided" do
+        @valid_test_record.delete(:email)
+        user = User.new(@valid_test_record)
+        expect(user.save).to eq(false)
+      end
+
+      it "fails if no name is provided" do
+        @valid_test_record.delete(:name)
+        user = User.new(@valid_test_record)
+        expect(user.save).to eq(false)
+      end
+
+      it "fails if no password is provided" do
+        @valid_test_record.delete(:password)
+        @valid_test_record.delete(:password_confirmation)
+        user = User.new(@valid_test_record)
+        expect(user.save).to eq(false)
+      end
+
+      it "fails when passwords do not match" do
+        user = User.new(@valid_test_record)
+        user.password_confirmation = "different"
+        expect(user.save).to eq(false)
+      end
+
+      it "fails when the password is shorter than 8 characters" do
+        @valid_test_record[:password] = "tooshrt"
+        @valid_test_record[:password_confirmation] = "tooshrt"
+        user = User.new(@valid_test_record)
+        expect(user.save).to eq(false)
+      end
+
+      it "fails if the email already exists (case-insensitive)" do
+        User.create(@valid_test_record)
+        existing_user = User.find_by(email: "bob@gmail.com")
+        expect(existing_user.email).to eq("bob@gmail.com")
+
+        @valid_test_record[:email] = "BOB@gmail.com"
+        new_user = User.new(@valid_test_record)
+        expect(new_user.save).to eq(false)        
+      end
+
+    end
+
+
+    describe ".authenticate_with_credentials" do
+
+      before do
+        User.create(@valid_test_record)
+        existing_user = User.find_by(email: "bob@gmail.com")
+        expect(existing_user.email).to eq("bob@gmail.com")
+      end
+
+      it "returns a user when given an existing email and valid password" do
+        result = User.authenticate_with_credentials("bob@gmail.com", "password")
+        expect(result).to be_a(User)         
+      end
+
+      it "returns nil when given an invalid email/password combination" do
+        result = User.authenticate_with_credentials("bob@gmail.com", "wrong")
+        expect(result).to eq(nil)         
+      end
+
+      it "returns nil when given a valid email but no password" do
+        result = User.authenticate_with_credentials("bob@gmail.com", "")
+        expect(result).to eq(nil)         
+      end
+
+      it "returns nil when given a neither an email nor a password" do
+        result = User.authenticate_with_credentials("", "")
+        expect(result).to eq(nil)         
+      end
+
+    end
+
+  end
+
+end
